aba5185712565c5ad73ddb7e9409712f34d1f964
nie.chao <nie.chao@advantech.com.cn>
2025-09-28 16:56:38 +0800

    github-runner Dockerfile

---
+++ github/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Runner 配置标识目录
+RUNNER_CONFIG_DIR="/tmp/runner/.runner"
+
+# 如果未配置过，执行 config.sh
+if [ ! -d "$RUNNER_CONFIG_DIR" ]; then
+  echo "Runner not configured, running config.sh..."
+  ./config.sh --url "$RUNNER_ORG_URL" --token "$RUNNER_TOKEN" --unattended
+else
+  echo "Runner already configured, skipping config.sh."
+fi
+
+# 永久运行 runner
+exec ./run.sh
