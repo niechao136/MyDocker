66228895a090a5e3137971661bdc34b024e43009
nie.chao <nie.chao@advantech.com.cn>
2026-02-04 13:45:24 +0800

    add qdrant

--- github/action-runner.sh
+++ github/action-runner.sh
@@ -1,6 +1,9 @@
-docker run -it --rm \
+docker run -d \
+  --name github-runner \
+  --restart unless-stopped \
+  -e RUNNER_NAME=my-runner \
   -e RUNNER_WORKDIR=/tmp/runner \
   -v runner-workdir:/tmp/runner \
   -v /var/run/docker.sock:/var/run/docker.sock \
   ghcr.io/actions/actions-runner:latest \
-  bash -c "./config.sh --url https://github.com/nechao136 --token AHV7KLSSPSG543B5WILLJBTI3DRXS --unattended && ./run.sh"
+  bash -c "./config.sh --url https://github.com/nechao136 --token AHV7KLX7BSA3GRNRNLTNJQTI3D62S --unattended && ./run.sh"
